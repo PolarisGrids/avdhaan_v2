a5888567ba58c9264df8ee65b1a13d2daa8aed89
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 <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>
2024-09-16 15:25:06 +0530

    fixing error

--- src/store/hes/types/index.ts
+++ src/store/hes/types/index.ts
@@ -26,24 +26,6 @@ export type ResponseBaseWithOutPagination<T> = {
   };
 };
 
-// export interface DeviceSimInfo {
-//   tspName: string;
-//   simNo: string;
-//   imsiNumber: string;
-//   iccid: string;
-//   ipv6Address: string;
-//   port: number;
-// }
-
-//  export type UpdateDevicePayload = {
-//   simDetails: {
-//     primarySimInfo?: DeviceSimInfo | null;
-//     secondarySimInfo?: DeviceSimInfo | null;
-//   };
-//   communicationProtocol?: string; // Should be "TAP" or "DLMS"
-//   deviceIdentifier: string;
-// };
-
 export type DeviceMetaInfoMetricsResponse =
   ResponseBaseWithOutPagination<DeviceMetaInfoMetricsRecord>;
 export type LocationHierarchyResponse =
